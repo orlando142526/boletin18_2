50ebabbf79f5d487c598ae16af541fe650e5178c
DANIELCASTELAO\oyagualendara <oyagualendara@daniecastelao.org>
2020-02-04 08:59:27 +0100

    ejercicio 2 del boletin 18

---
+++ boletin18_2/Boletin18_2.java
@@ -0,0 +1,18 @@
+
+package boletin18_2;
+
+
+public class Boletin18_2 {
+
+    
+    public static void main(String[] args) {
+        // TODO code application logic here
+ Metodos objeto=new Metodos();
+    int[] notas=new int[30];
+
+    notas=objeto.crearArray(notas);
+    objeto.visualizarGrupos(notas);
+    objeto.calcularMedia(notas);
+    objeto.notaAlta(notas);
+}   
+}
